0fd4cf987dc70df4e4988606076c348cba178344
Mic <mic@nomaster.cc>
2016-01-14 12:22:33 +0100

    Fix wordpress missing configuration

--- wordpress-config.php
+++ wordpress-config.php
@@ -3,11 +3,14 @@
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'wordpress');
-define('DB_HOST', '192.168.0.3');
+define('DB_HOST', '{{ hostvars['db']['ansible_eth1']['ipv4']['address'] }}');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
+
+define('AUTH_KEY','{{ lookup('password', 'wp_auth_key', length='64') }}');
+
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
